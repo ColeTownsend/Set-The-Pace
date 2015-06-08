8025f1cd877ac281566dd94abb0c7f05d6b3e243
Cole Townsend <cole@coletownsend.com>
2015-06-08 16:14:00 -0400

    pages.

--- gulpfile.js
+++ gulpfile.js
@@ -37,13 +37,13 @@ gulp.task("jekyll:prod", $.shell.task("jekyll build --config _config.yml,_config
 // Compiles the SASS files and moves them into the "assets/stylesheets" directory
 gulp.task("styles", function () {
   // Looks at the style.scss file for what to include and creates a style.css file
-  return gulp.src("src/assets/scss/*.scss")
+  return gulp.src("./assets/scss/*.scss")
     .pipe($.sass())
     // AutoPrefix your CSS so it works between browsers
     .pipe($.autoprefixer("last 1 version", { cascade: true }))
     // Directory your CSS file goes to
-    .pipe(gulp.dest("src/assets/stylesheets/"))
-    .pipe(gulp.dest("serve/assets/stylesheets/"))
+    .pipe(gulp.dest("./assets/stylesheets/"))
+    .pipe(gulp.dest("./assets/stylesheets/"))
     // Outputs the size of the CSS file
     .pipe($.size({title: "styles"}))
     // Injects the CSS changes to your browser since Jekyll doesn"t rebuild the CSS
@@ -52,30 +52,23 @@ gulp.task("styles", function () {
 
 // Optimizes the images that exists
 gulp.task("images", function () {
-  return gulp.src("src/assets/images/**")
-    .pipe($.changed("site/assets/images"))
+  return gulp.src("./assets/images/**")
+    .pipe($.changed("./assets/images"))
     .pipe($.imagemin({
       // Lossless conversion to progressive JPGs
       progressive: true,
       // Interlace GIFs for progressive rendering
       interlaced: true
     }))
-    .pipe(gulp.dest("site/assets/images"))
+    .pipe(gulp.dest("./assets/images"))
     .pipe($.size({title: "images"}));
 });
 
 // Copy over fonts to the "site" directory
 gulp.task("fonts", function () {
-  return gulp.src("src/assets/fonts/**")
-    .pipe(gulp.dest("site/assets/fonts"))
+  return gulp.src("./assets/fonts/**")
+    .pipe(gulp.dest("./assets/fonts"))
     .pipe($.size({ title: "fonts" }));
-});
-
-// Copy xml and txt files to the "site" directory
-gulp.task("copy", function () {
-  return gulp.src(["serve/*.txt", "serve/*.xml"])
-    .pipe(gulp.dest("site"))
-    .pipe($.size({ title: "xml & txt" }))
 });
 
 // Optimizes all the CSS, HTML and concats the JS etc
@@ -175,10 +168,10 @@ gulp.task("check", ["jslint", "doctor"], function () {
 });
 
 // Builds the site but doesn"t serve it to you
-gulp.task("build", ["jekyll:prod", "styles"], function () {});
+gulp.task("build", ["styles"], function () {});
 
 // Builds your site with the "build" command and then runs all the optimizations on
 // it and outputs it to "./site"
 gulp.task("publish", ["build"], function () {
-  gulp.start("html", "copy", "images", "fonts");
+  gulp.start("html", "images", "fonts");
 });
